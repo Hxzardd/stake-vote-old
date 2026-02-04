23562f9d437724bc4e699a13b0d125c2536e3fec
Pranav Diddi <pranavdiddi@gmail.com>
2026-02-05 01:45:25 +0530

    contracts.ts_updated

--- contracts/frontend/src/contracts/contracts.ts
+++ contracts/frontend/src/contracts/contracts.ts
@@ -1,16 +1,19 @@
 import companySharesAbi from "./CompanySharesABI.json";
 import stakeVotingAbi from "./StakeWeightedVotingABI.json";
 
-// TEMP: Remix VM addresses (replace later with testnet addresses)
-export const COMPANY_SHARES_ADDRESS = "PASTE_COMPANY_SHARES_ADDRESS_HERE";
-export const STAKE_VOTING_ADDRESS = "PASTE_STAKE_WEIGHTED_VOTING_ADDRESS_HERE";
+// Remix VM addresses (TEMP — replace with testnet later)
+export const COMPANY_SHARES_ADDRESS =
+  "0xD7ACd2a9FD159E69Bb102A1ca21C9a3e3A5F771B";
+
+export const STAKE_VOTING_ADDRESS =
+  "0xDA0bAb807633f07F013f94DD0E6A4F96F8742B53";
 
 export const companySharesContract = {
-  address: 0xD7ACd2a9FD159E69Bb102A1ca21C9a3e3A5F771B,
+  address: COMPANY_SHARES_ADDRESS,
   abi: companySharesAbi,
 };
 
 export const stakeVotingContract = {
-  address: 0xDA0bab807633f07f013f94DD0E6A4F96F8742B53,
+  address: STAKE_VOTING_ADDRESS,
   abi: stakeVotingAbi,
 };
